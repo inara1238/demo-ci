e1a4a55c0e594742642c74886f95549b8b056e9e
Inara <inara.paula@estudante.ufla.br>
2023-07-07 09:40:01 -0300

    erro 01

--- src/main/java/br/ufmg/dcc/Calculadora.java
+++ src/main/java/br/ufmg/dcc/Calculadora.java
@@ -3,11 +3,11 @@ package br.ufmg.dcc;
 public class Calculadora {
 
   public int soma(int x, int y) {
-    return x + y;
+    return x + y +2;
   }
 
   public int subtrai(int x, int y) {
-    return x - y +2
+    return x - y ;
   }
 
 }
